e01e243307b8d04f4eaf1a0eaa99f4c5ac9fe9d7
Anirudh G <anirudhyamunang@gmail.com>
2021-03-01 11:05:59 +0100

    added states

--- src/qui.cc
+++ src/qui.cc
@@ -10,31 +10,31 @@ static bool done = false;
 
 int main(int argc, char* argv[])
 {
+  // Setup
   char* openfile;
   std::vector <std::string> x;
   std::string input;
   
-  if (argc != 2)
-    {
-      USAGE;
-      exit(0);
-    }
+  // Arguments
+  if (argc != 2) goto Use;
   else if (argv[1] == "-h" || argv[1] == "--help")
-    std::cout << colours << purple << colour << "qui <FILE>" << std::endl;
+    goto Help;
   else openfile = argv[1];
 
+  // Opening file
   FILE* OpenFile;
   OpenFile = fopen(openfile, "w+");
   std::cout << colours << yellow << colour << "Opening file: " << openfile << std::endl;
   fputs("\n", OpenFile);
-  
+
+  // Mainloop
   while (!done)
     {
       std::cout << colours << white << colour << "";
       std::cin >> input;
-      if (input == ":q!") done = true;
+      if      (input == ":q!")    goto End;
       else if (input == ":less!") Handle::less(openfile);
-      else if (input == ":n!") fputs("\n", OpenFile);
+      else if (input == ":n!")    fputs("\n", OpenFile);
       else if (input == ":size!") Handle::size(OpenFile);
       else if (input == ":cat!")  Handle::cat(openfile);
       else
@@ -43,12 +43,20 @@ int main(int argc, char* argv[])
 	  fputs(input.c_str(), OpenFile);
 	}   
     }
-  
+
+
+End: // End
   fclose(OpenFile);
   std::cout << colours << green << colour << "Wrote: ";
-  for (std::string u: x)
-    std::cout << u;
+  for (std::string u: x) std::cout << u;
   std::cout << " to file: " << openfile << std::endl;
-  
+
   return 0;
+Use: // Usage
+  USAGE;
+  exit(0);
+Help: // Help
+  std::cout << colours << purple << colour << "qui <FILE>" << std::endl;
+  
 }
+
